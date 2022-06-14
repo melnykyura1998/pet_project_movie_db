309b2e0d7edbec323160e118b34a44c7bae61ac0
melnykyura1998 <melnykyura1998@gmail.com>
2022-06-14 09:33:20 +0300

    add videos and else

--- src/components/singleActor/filmsWithActor.js
+++ src/components/singleActor/filmsWithActor.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {actorsService} from "../../services/actors.service";
 
-import {Loader} from "../../UI";
 
 import MovieCard from "./movieCard";
 
@@ -10,11 +9,12 @@ const FilmsWithActor = ({actorId}) => {
     useEffect(()=>{
        actorsService.getFilm(actorId).then(({data})=>setCast(data.cast.slice(0,5).filter(item => item.poster_path)))
     },[actorId])
+    console.log(cast)
     return (
         <div>
-            <h3>Movies with the actor</h3>
+            {cast.length > 0 ? <h3>Movies with the actor</h3>:''}
             <div style={{display: "flex"}}>
-                {cast.length > 0 ? cast.map(movie => <MovieCard key={movie.credit_id} movie={movie}/>) : <Loader/>}
+                {cast.length > 0 ? cast.map(movie => <MovieCard key={movie.credit_id} movie={movie}/>) : ''}
             </div>
         </div>
     );
